fc7b24b04b6f6af347c60b9388b1d5faa725c653
parag1267 <hadiyaparag40@gmail.com>
2025-11-13 10:07:19 +0530

    Fetch Api

--- Fetch_Api/javascript.js
+++ Fetch_Api/javascript.js
@@ -1,4 +1,4 @@
-fetch("https://fakestoreapi.com/users/1")
+fetch("https://fakestoreapi.com/users")
 .then((response) => response.json())
 .then((data) => console.log(data))
 .catch((error) => console.log(error));
